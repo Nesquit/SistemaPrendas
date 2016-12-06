a3f0114352e73982435e5c790793bc0cfbb8db3a
callo <callo@Kayito-Acer>
2016-12-05 23:39:37 -0600

    Proveedor ABC

--- src/java/beans/ProveedorBean.java
+++ src/java/beans/ProveedorBean.java
@@ -5,21 +5,130 @@
  */
 package beans;
 
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import dao.ProveedorDAO;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import modelos.Proveedor;
 
 /**
  *
  * @author nesquit
  */
-@Named(value = "proveedorBean")
-@Dependent
+@ManagedBean
+@ViewScoped
 public class ProveedorBean {
 
+    private Proveedor proveedor=new Proveedor();
+    private List<Proveedor> listaProveedores;
+    private String status;
     /**
      * Creates a new instance of ProveedorBean
      */
     public ProveedorBean() {
     }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public List<Proveedor> getListaProveedores() {
+        return listaProveedores;
+    }
+
+    public void setListaProveedores(List<Proveedor> listaProveedores) {
+        this.listaProveedores = listaProveedores;
+    }
+    
+    
+    public void registrar() throws Exception{
+        ProveedorDAO dao;
+        try {
+            dao=new ProveedorDAO();
+            System.out.println("1");
+            dao.registrar(proveedor);
+           
+            System.out.println("2");
+        } catch (Exception e) {
+            System.out.println("3");
+            throw  e;
+            
+        }
+        
+    }
+    
+     public  void listar() throws Exception{
+        ProveedorDAO dao;
+        try {
+            dao=new ProveedorDAO();
+            listaProveedores=dao.Listar();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    public void boton(){
+        try {
+            registrar();
+            listar();
+        } catch (Exception ex) {
+            Logger.getLogger(ProveedorBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    public  void leerID(Proveedor prov) throws Exception{
+        Proveedor temp;
+        ProveedorDAO dao;
+        
+        try {
+            dao=new ProveedorDAO();
+            temp=dao.leerID(prov);
+            if (temp!=null) {
+                this.proveedor=temp;                
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    public void clean(){
+        proveedor=null;
+    }
+    
+    public void modificar()throws Exception{
+        ProveedorDAO dao;
+        try {
+            dao=new ProveedorDAO();
+            dao.modificar(proveedor);
+            this.listar();
+        } catch (Exception e) {
+        }
+    }
+    
+    public void eliminar(Proveedor prov) throws Exception{
+        ProveedorDAO dao;
+        try {
+            dao=new ProveedorDAO();
+            dao.eliminar(prov);
+            this.listar();
+        } catch (Exception e) {
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
     
 }
--- src/java/dao/ProveedorDAO.java
+++ src/java/dao/ProveedorDAO.java
@@ -5,10 +5,167 @@
  */
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import modelos.Proveedor;
+
 /**
  *
  * @author nesquit
  */
-public class ProveedorDAO {
+public class ProveedorDAO extends DAO{
+        
+    
+    public boolean registrar(Proveedor prov) throws Exception {
+       boolean r=true;
+        
+        try {
+            this.conectar();
+            PreparedStatement st = this.getCon()
+                    .prepareStatement("INSERT INTO `proveedores` "
+                            + "(`nombreEmpresa`,"
+                            + " `nombreContacto`,"
+                            + " `apellidoContacto`,"
+                            + " `direccion`, "
+                            + "`telefono`, "
+                            + "`correo`,"
+                            + " `estatus`) VALUES (?, ?, ?, ?,  ?, ?, ?)");
+
+            st.setString(1, prov.getNombreEmpresa());
+            st.setString(2, prov.getNombreContacto());
+            st.setString(3, prov.getApellidoContacto());
+            st.setString(4, prov.getDireccion());
+            st.setString(5, prov.getTelefono());
+            st.setString(6, prov.getCorreo());
+            st.setInt(7, prov.getEstatus());
+            st.executeUpdate();
+            System.out.println(st.getUpdateCount());
+        } catch (Exception e) {
+            System.out.println("error: jeje" + e);
+            r=false;
+            System.out.println("hola");
+            
+            
+        } finally {
+            this.cerrar();
+        }
+        System.out.println("jej "+r);
+        return r;
+    }
+    
+    
+    public List<Proveedor> Listar() throws Exception{
+        List<Proveedor> lista;
+        ResultSet rs;
+        try {
+            this.conectar();
+            PreparedStatement st=this.getCon().prepareStatement("select * from proveedores");
+            rs=st.executeQuery();
+            lista=new ArrayList();
+            while (rs.next()) {
+                Proveedor prov=new Proveedor();
+                prov.setIdProveedor(rs.getInt("id"));
+                prov.setNombreEmpresa(rs.getString("nombreEmpresa"));
+                prov.setNombreContacto(rs.getString("nombreContacto"));
+                prov.setApellidoContacto(rs.getString("apellidoContacto"));
+                prov.setDireccion(rs.getString("direccion"));
+                prov.setTelefono(rs.getString("telefono"));
+                prov.setCorreo(rs.getString("correo"));
+                prov.setEstatus(rs.getInt("estatus"));
+                if (rs.getInt("estatus")==1) {
+                    prov.setEstatusImg("./estatus/verde.png");
+                }else if (rs.getInt("estatus")==2 || rs.getInt("estatus")==0 ) {
+                    prov.setEstatusImg("./estatus/rojo.png");                    
+                }
+                
+                lista.add(prov);
+            }
+        } catch (Exception e) {
+            throw e;
+        }finally{
+            this.cerrar();
+        }
+        return lista;
+    }
+    
+    public Proveedor leerID(Proveedor prove) throws Exception{
+        Proveedor prov=null;
+        ResultSet rs;
+        
+            try {
+            this.conectar();
+            PreparedStatement st=this.getCon().prepareStatement("select * from proveedores where id=?");
+            st.setInt(1, prove.getIdProveedor());
+            rs=st.executeQuery();
+            
+            if (rs.next()) {
+                prov=new Proveedor();
+                 prov.setIdProveedor(rs.getInt("id"));
+                 prov.setNombreEmpresa(rs.getString("nombreEmpresa"));
+                prov.setNombreContacto(rs.getString("nombreContacto"));
+                prov.setApellidoContacto(rs.getString("apellidoContacto"));
+                prov.setDireccion(rs.getString("direccion"));
+                prov.setTelefono(rs.getString("telefono"));
+                prov.setCorreo(rs.getString("correo"));
+                prov.setEstatus(rs.getInt("estatus"));
+             
+            }
+        } catch (Exception e) {
+            throw e;
+        }finally{
+            this.cerrar();
+        }
+        return prov;
+        
+    }
+    
+    
+    public void modificar(Proveedor prov)throws Exception{
+        try {
+          
+            
+            this.conectar();
+            PreparedStatement st=this.getCon().prepareStatement("update proveedores set "
+                    + "nombreEmpresa=?,"
+                    + "nombreContacto=?,"
+                    + "apellidoContacto=?,"
+                    + "direccion=?, "
+                    + "telefono=? ,"
+                    + "correo=? ,"
+                    + "estatus=? "
+                    + "where id=?");
+            st.setString(1, prov.getNombreEmpresa());
+            st.setString(2, prov.getNombreContacto());
+            st.setString(3, prov.getApellidoContacto());
+            st.setString(4, prov.getDireccion());
+            st.setString(5, prov.getTelefono());
+            st.setString(6, prov.getCorreo());
+            st.setInt(7, prov.getEstatus());
+            st.setInt(8, prov.getIdProveedor());
+            st.executeUpdate();
+            
+        } catch (Exception e) {
+            throw  e;
+        }finally{
+            this.cerrar();
+        }
+    }
+     public void eliminar(Proveedor prov) throws Exception{
+         try {
+             this.conectar();
+             PreparedStatement st=this.getCon().prepareStatement("delete from proveedores where id=?");
+             st.setInt(1, prov.getIdProveedor());
+             st.executeUpdate();
+             
+         } catch (Exception e) {
+             throw e;
+         }finally{
+             this.cerrar();
+         }
+         
+     }
+    
     
 }
